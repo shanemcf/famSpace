125fc313379441dcc82bc45d4d5abb601620c1f4
Jessica Jernigan <jernigan.jessica.leigh@gmail.com>
2020-08-29 13:09:59 -0500

    establishing file structure

---
+++ controllers/api/index.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+
+const famRoutes = require('./fam-routes.js');
+const userRoutes = require('./user-routes.js');
+const postRoutes = require('./post-routes');
+const commentRoutes = require('./comment-routes');
+
+router.use('/fams', famRoutes);
+router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes);
+
+module.exports = router;
