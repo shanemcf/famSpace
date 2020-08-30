6428177e144f2fbf60263fead94996b8cededf8f
josephptflanagan <josephptflanagan@gmail.com>
2020-08-30 09:53:51 -0500

    added sign-up route

--- controllers/post-feed-routes.js
+++ controllers/post-feed-routes.js
@@ -114,5 +114,15 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
+// sign up
+router.get('/sign-up', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('sign-up');
+});
+
 
 module.exports = router;
