caba45c406d4d8977ef37409c3b92e1078bb8ddd
josephptflanagan <josephptflanagan@gmail.com>
2020-08-29 22:46:13 -0500

    updated server, fixed error in readme

--- server.js
+++ server.js
@@ -1,12 +1,35 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers');
+const path = require('path');
 const sequelize = require('./config/connection');
+const session = require('express-session');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
+//Pull in secure local data
+require('dotenv').config();
+
+//Session creation
+const sess = {
+  secret: process.env.SECRET,
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+//Server and Port Setup
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+//Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.use(session(sess));
 
 // turn on routes
 app.use(routes);
