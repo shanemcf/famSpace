f0dd7cdfa5dabaa673f24b6f828274a71e21f4b8
josephptflanagan <josephptflanagan@gmail.com>
2020-08-29 23:14:25 -0500

    moving to skeleton

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const routes = require('./controllers');
 const path = require('path');
 const sequelize = require('./config/connection');
+const helpers = require('./utils/helpers');
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({ helpers });
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
