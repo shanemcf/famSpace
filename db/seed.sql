0dd5f066e6ecc3b956efd460378a248c30e27eca
josephptflanagan <josephptflanagan@gmail.com>
2020-09-01 18:30:55 -0500

    started seed

---
+++ db/seed.sql
@@ -0,0 +1,9 @@
+INSERT INTO fam (famKey)
+VALUES
+('crazyCostanzas26'),
+('happySeinfelds45');
+
+INSERT INTO user (first_name, last_name, birthdate, email, username, personal_password, fam_id)
+VALUES
+('Frank', 'Costanza', '1937-06-27 05:00', 'fCostanza@fakeMail.com', 'fCostanza', '$erenityNow', 0);
+
