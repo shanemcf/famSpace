85ff38cf22a977720461830d8a168ea7bd2f1161
ryannekillian <ryanne.killian@gmail.com>
2020-08-31 21:14:46 -0500

    upload functionality

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,10 +4,12 @@ const famRoutes = require('./fam-routes.js');
 const userRoutes = require('./user-routes.js');
 const postRoutes = require('./post-routes');
 const commentRoutes = require('./comment-routes');
+const uploadRoutes = require('./upload-routes');
 
 router.use('/fams', famRoutes);
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
 router.use('/comments', commentRoutes);
+router.use('/upload', uploadRoutes);
 
 module.exports = router;
