3324c5f712738a40411ae81149d84fbeade00862
Jessica Jernigan <jernigan.jessica.leigh@gmail.com>
2020-09-02 18:31:36 -0500

    can add comments to the first post on the page, but not others

--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -1,34 +1,31 @@
-// This still needs work -- grabbed it from a previous project.
-
 async function commentFormHandler(event) {
   event.preventDefault();
 
-  const comment_text = document
-    .querySelector('textarea[name="comment-body"]')
-    .value.trim();
+  const comment_text = document.querySelector('textarea[name="comment-body"]').value.trim();
 
-  const post_id = window.location.toString().split('/')[
-    window.location.toString().split('/').length - 1
-  ];
+  let post_id = document.getElementsByTagName("custom")[0].getAttribute("postId");
+  
+  console.log('post-id: ', post_id)
 
-  if (comment_text) {
-    const response = await fetch('/api/comments', {
-      method: 'POST',
-      body: JSON.stringify({
-        post_id,
-        comment_text
-      }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-
-    if (response.ok) {
-      document.location.reload();
-    } else {
-      alert(response.statusText);
-    }
-  }
+  // if (comment_text) {
+  //   const response = await fetch('/api/comments', {
+  //     method: 'POST',
+  //     body: JSON.stringify({
+  //       post_id,
+  //       comment_text
+  //     }),
+  //     headers: {
+  //       'Content-Type': 'application/json'
+  //     }
+  //   });
+  
+  //   if (response.ok) {
+  //     document.location.reload();
+  //   } else {
+  //     alert(response.statusText);
+  //   }
+  // }
+  
 }
 
 document.querySelector('.comment-form').addEventListener('submit', commentFormHandler);
