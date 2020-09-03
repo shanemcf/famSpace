bdeb02976764cc7c37d5dc67fecad64db80556db
Jessica Jernigan <jernigan.jessica.leigh@gmail.com>
2020-09-02 20:10:05 -0500

    progress on comments, but still isn't working

--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -3,29 +3,29 @@ async function commentFormHandler(event) {
 
   const comment_text = document.querySelector('textarea[name="comment-body"]').value.trim();
 
-  let post_id = document.getElementsByTagName("custom")[0].getAttribute("postId");
-  
-  console.log('post-id: ', post_id)
+  // const post_id = document.getElementById("post-id").innerHTML;
+  const post_id = $(this).attr("data-id");
+  console.log('this is post_id: ', post_id)
 
-  // if (comment_text) {
-  //   const response = await fetch('/api/comments', {
-  //     method: 'POST',
-  //     body: JSON.stringify({
-  //       post_id,
-  //       comment_text
-  //     }),
-  //     headers: {
-  //       'Content-Type': 'application/json'
-  //     }
-  //   });
+  if (comment_text) {
+    const response = await fetch('/api/comments', {
+      method: 'POST',
+      body: JSON.stringify({
+        post_id,
+        comment_text
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
   
-  //   if (response.ok) {
-  //     document.location.reload();
-  //   } else {
-  //     alert(response.statusText);
-  //   }
-  // }
+    if (response.ok) {
+      document.location.reload();
+    } else {
+      alert(response.statusText);
+    }
+  }
   
 }
 
-document.querySelector('.comment-form').addEventListener('submit', commentFormHandler);
+document.querySelector('.cmt-btn').addEventListener('click', commentFormHandler);
