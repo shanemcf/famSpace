de00acf1cdbba2a249895d69faa85397fe747442
Jessica Jernigan <jernigan.jessica.leigh@gmail.com>
2020-09-05 18:05:56 -0500

    added New Post functionality -- imageURL is hardcoded at the moment

---
+++ public/javascript/add-post.js
@@ -0,0 +1,29 @@
+async function newFormHandler(event) {
+  event.preventDefault();
+
+  console.log('The eventListener is working')
+
+  const caption = document.querySelector('textarea[name="caption"]').value;
+  const imageURL = 'www.url.com'
+
+  console.log('Here are the caption and img_path: ', caption, imageURL)
+  
+  const response = await fetch(`/api/posts`, {
+    method: 'POST',
+    body: JSON.stringify({
+      caption,
+      imageURL
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+
+  if (response.ok) {
+    document.location.replace('/dashboard');
+  } else {
+    alert(response.statusText);
+  }
+}
+
+document.querySelector('#add-post').addEventListener('click', newFormHandler);
