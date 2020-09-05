0c9418e7cc69a4df372cc3131fe7244975651f87
josephptflanagan <josephptflanagan@gmail.com>
2020-09-05 18:23:34 -0500

    image upload functional

--- controllers/api/upload-routes.js
+++ controllers/api/upload-routes.js
@@ -1,6 +1,5 @@
 const router = require('express').Router();
 const fs = require('fs');
-// 5429e53411446494f4e1d0135d038c5b62c0a8c0
 const cors = require('cors');
 
 router.post("/", cors(), (req, res) => {
@@ -13,25 +12,23 @@ router.post("/", cors(), (req, res) => {
           message: 'No file uploaded',
         });
       } else {
-        console.log("req.files.displayImage:", req.files.displayImage)
-        //Use the name of the input field (i.e. "displayImage") to retrieve the uploaded file
-        let displayImage = req.files.displayImage;
-        
-        //let photoAddress = './uploads/' + displayImage.name
-  
+
+        let image = req.files.file;
+        let imageName = req.files.file.name;
+
+        let photoAddress = './uploads/' + imageName
+
         //Use the mv() method to place the file in upload directory (i.e. "uploads")
-        displayImage.mv('./uploads/' + displayImage.name);
-        console.log("entered else")        
-        // router.put("/user/" + req.session.user_id)
+        image.mv(photoAddress);
 
         //send response
         res.send({
           status: 200,
           message: 'File is uploaded',
           data: {
-            name: displayImage.name,
-            mimetype: displayImage.mimetype,
-            size: displayImage.size,
+            name:image.name,
+            mimetype: image.mimetype,
+            size: image.size,
           },
         });
       }
