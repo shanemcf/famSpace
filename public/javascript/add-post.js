66f3d9127f33730768f13255abed285d2f81b9fe
Jessica Jernigan <jernigan.jessica.leigh@gmail.com>
2020-09-06 18:10:54 -0500

    made dropzone function async so it would play nice

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -25,15 +25,20 @@
 // }
 
 // document.querySelector('#add-post').addEventListener('click', newFormHandler);
+
+
 var myDropzone = new Dropzone(document.getElementById('dropzone-area'), {
   uploadMultiple: false,
   acceptedFiles: '.jpg,.png,.jpeg,.gif',
   parallelUploads: 6,
   url: 'https://api.cloudinary.com/v1_1/famspace/image/upload'
 });
+
+
 myDropzone.on('sending', function (file, xhr, formData) {
   alert("you added a post");
 });
+
 
 myDropzone.on('sending', function (file, xhr, formData) {
   console.log("Adding api key ", 615937344959933);
@@ -41,19 +46,21 @@ myDropzone.on('sending', function (file, xhr, formData) {
   formData.append('timestamp', Date.now() / 1000 | 0);
   formData.append('upload_preset', 'iftxut2f');
 });
-myDropzone.on('success', function (file, response) {
+
+
+myDropzone.on('success', async function (file, response) {
   console.log('response: ', response);
   let cloudinaryURL = "https://res.cloudinary.com/famspace/image/upload/v" + response.version + "/" + response.public_id + ".jpg"
   console.log("cloudinaryURL: ", cloudinaryURL);
   if (cloudinaryURL) {
     const response = await fetch('/api/upload/newPost', {
       method: 'POST',
-      body: JSON.stringify({
-        cloudinaryURL
-      }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
+         body: {
+           cloudinaryURL:cloudinaryURL
+         },
+         headers: {
+           'Content-Type': 'application/json'
+         } 
     });
     console.log('This is the response: ', response)
     if (response.ok) {
@@ -63,10 +70,10 @@ myDropzone.on('success', function (file, response) {
       alert(response.statusText);
     }
   }
-    
-    //console.log('response.version:', response.version)
-    //console.log('Success! uploading file to Cloudinary. public id - '+response.public_id );
-  });
+
+  console.log('response.version:', response.version)
+  console.log('Success! uploading file to Cloudinary. public id - ' + response.public_id);
+});
 
 
 
